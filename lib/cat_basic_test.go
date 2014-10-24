fa26b5759eee7c0dcc9f4c8ab397aace2ba7e01b
yarbelk <james.rivettcarnac@gmail.com>
2014-10-24 15:03:05 +0800

    finished basic functionality of the simple program

---
+++ lib/cat_basic_test.go
@@ -0,0 +1,110 @@
+package cat
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+// Catting an empty file should result in an empty response
+func TestNoLineCat(t *testing.T) {
+	var inputValue string = ""
+	var inputReader = strings.NewReader(inputValue)
+	var outputStream bytes.Buffer
+	var expectedValue string = ""
+
+	Cat(inputReader, &outputStream)
+
+	recievedValue := outputStream.String()
+
+	if expectedValue != recievedValue {
+		t.Fatalf("Expected %q, was %q", expectedValue, recievedValue)
+	}
+
+}
+
+// A File with one line and a new line should be the same
+func TestOneLineCat(t *testing.T) {
+	var inputValue string = "Hello World!\n"
+	var inputReader = strings.NewReader(inputValue)
+	var outputStream bytes.Buffer
+	var expectedValue string = "Hello World!\n"
+
+	Cat(inputReader, &outputStream)
+
+	recievedValue := outputStream.String()
+
+	if expectedValue != recievedValue {
+		t.Fatalf("Expected %q, was %q", expectedValue, recievedValue)
+	}
+}
+
+// A file without a newline at the end should also have a newline
+func TestOneLineCatNoNewline(t *testing.T) {
+	var inputValue string = "Hello World!"
+	var inputReader = strings.NewReader(inputValue)
+	var outputStream bytes.Buffer
+	var expectedValue string = "Hello World!\n"
+
+	Cat(inputReader, &outputStream)
+
+	recievedValue := outputStream.String()
+
+	if expectedValue != recievedValue {
+		t.Fatalf("Expected %q, was %q", expectedValue, recievedValue)
+	}
+}
+
+// A with multiple lines should be output on multiiple lines
+func TestMultiLineCat(t *testing.T) {
+	var inputValue string = "Hello World!\n\nHello Gophers!\n"
+	var inputReader = strings.NewReader(inputValue)
+	var outputStream bytes.Buffer
+	var expectedValue string = "Hello World!\n\nHello Gophers!\n"
+
+	Cat(inputReader, &outputStream)
+
+	recievedValue := outputStream.String()
+
+	if expectedValue != recievedValue {
+		t.Fatalf("Expected %q, was %q", expectedValue, recievedValue)
+	}
+}
+
+// Calling cat twice should concatinate the results
+func TestMultiInputCat(t *testing.T) {
+	var inputValueOne string = "Hello World!\n"
+	var inputValueTwo string = "Hello Gophers!\n"
+	var inputReaderOne = strings.NewReader(inputValueOne)
+	var inputReaderTwo = strings.NewReader(inputValueTwo)
+	var outputStream bytes.Buffer
+	var expectedValue string = "Hello World!\nHello Gophers!\n"
+
+	Cat(inputReaderOne, &outputStream)
+	Cat(inputReaderTwo, &outputStream)
+
+	recievedValue := outputStream.String()
+
+	if expectedValue != recievedValue {
+		t.Fatalf("Expected %q, was %q", expectedValue, recievedValue)
+	}
+}
+
+// Calling cat twice should concatinate the results with a newline if it is missing
+func TestMultiInputCatMissingNewline(t *testing.T) {
+	var inputValueOne string = "Hello World!"
+	var inputValueTwo string = "Hello Gophers!\n"
+	var inputReaderOne = strings.NewReader(inputValueOne)
+	var inputReaderTwo = strings.NewReader(inputValueTwo)
+	var outputStream bytes.Buffer
+	var expectedValue string = "Hello World!\nHello Gophers!\n"
+
+	Cat(inputReaderOne, &outputStream)
+	Cat(inputReaderTwo, &outputStream)
+
+	recievedValue := outputStream.String()
+
+	if expectedValue != recievedValue {
+		t.Fatalf("Expected %q, was %q", expectedValue, recievedValue)
+	}
+}
