28439c5fe5103f6517dab99e561bc88e5a584fd2
James Rivett-Carnac <james.rivettcarnac@gmail.com>
2021-06-06 12:26:53 +0200

    add wc to main.go badly
     - need to migrate everything to pflags

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"syscall"
 
+	"github.com/spf13/pflag"
 	"github.com/voxelbrain/goptions"
 	"github.com/yarbelk/slimbox/lib"
 	"github.com/yarbelk/slimbox/lib/cat"
 	"github.com/yarbelk/slimbox/lib/falsy"
 	"github.com/yarbelk/slimbox/lib/truthy"
+	"gitlab.com/yarbelk/slimbox/lib/wc"
 )
 
 var catOptions cat.CatOptions
@@ -23,6 +26,7 @@ type Options struct {
 	Cat   cat.CatOptions     `goptions:"cat"`
 	False falsy.FalseOptions `goptions:"false"`
 	True  truthy.TrueOptions `goptions:"true"`
+	Wc    struct{}           `goptions:"wc"`
 }
 
 func version() {
@@ -83,35 +87,51 @@ func main() {
 	flagset := goptions.NewFlagSet(filepath.Base(os.Args[0]), &options)
 	err := flagset.Parse(os.Args[1:])
 
-	if err != nil {
-		if options.Verb == "cat" {
-			var catHelp goptions.HelpFunc = goptions.NewTemplatedHelpFunc(cat.CAT_HELP)
-			flagset.Verbs["cat"].HelpFunc = catHelp
-			flagset.Verbs["cat"].PrintHelp(os.Stderr)
-			os.Exit(1)
-		} else if options.Verb == "false" {
-			var falseHelp goptions.HelpFunc = goptions.NewTemplatedHelpFunc(falsy.FALSE_HELP)
-			flagset.Verbs["false"].HelpFunc = falseHelp
-			flagset.Verbs["false"].PrintHelp(os.Stderr)
-			os.Exit(1)
-		} else if options.Verb == "True" {
-			var trueHelp goptions.HelpFunc = goptions.NewTemplatedHelpFunc(truthy.TRUE_HELP)
-			flagset.Verbs["true"].HelpFunc = trueHelp
-			flagset.Verbs["true"].PrintHelp(os.Stderr)
+	// This is all placeholder until i migrate true, false and cat to pflag (fix argument ordering)
+	switch options.Verb {
+	case "cat":
+		if err == nil {
+			runCat(&options)
+			os.Exit(0)
+		}
+		var catHelp goptions.HelpFunc = goptions.NewTemplatedHelpFunc(cat.CAT_HELP)
+		flagset.Verbs["cat"].HelpFunc = catHelp
+		flagset.Verbs["cat"].PrintHelp(os.Stderr)
+		os.Exit(1)
+	case "false":
+		if err == nil {
+			runFalse(&options)
+		}
+		var falseHelp goptions.HelpFunc = goptions.NewTemplatedHelpFunc(falsy.FALSE_HELP)
+		flagset.Verbs["false"].HelpFunc = falseHelp
+		flagset.Verbs["false"].PrintHelp(os.Stderr)
+		os.Exit(1)
+	case "true":
+		if err == nil {
+			runTrue(&options)
+		}
+		var trueHelp goptions.HelpFunc = goptions.NewTemplatedHelpFunc(truthy.TRUE_HELP)
+		flagset.Verbs["true"].HelpFunc = trueHelp
+		flagset.Verbs["true"].PrintHelp(os.Stderr)
+		os.Exit(1)
+	case "wc":
+		// Yup: I really hate this.
+		var options *pflag.FlagSet = pflag.NewFlagSet("wc", pflag.ContinueOnError)
+		options.BoolP(wc.LineFlag, "l", false, "Count newlines")
+		options.BoolP(wc.ByteFlag, "c", false, "Count bytes")
+		options.BoolP(wc.WordFlag, "w", false, "Count words")
+		options.BoolP(wc.CharFlag, "m", false, "Count characters")
+		options.BoolP(wc.LongFlag, "L", false, "Print longest line length")
+		if err := options.Parse(os.Args[2:]); err != nil {
+			fmt.Fprint(os.Stderr, err)
+			options.Usage()
+		}
+		if err := wc.WcMain(options); err != nil {
+			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-
+	default:
 		flagset.PrintHelp(os.Stderr)
 		os.Exit(1)
-	}
-
-	if options.Verb == "cat" {
-		runCat(&options)
-	} else if options.Verb == "false" {
-		runFalse(&options)
-	} else if options.Verb == "true" {
-		runTrue(&options)
-	} else {
-		flagset.PrintHelp(os.Stderr)
 	}
 }
